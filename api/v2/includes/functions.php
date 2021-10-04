178adc5d3f26a7e5db3b4ebab35a30eb679d9f85
memojl <memojl08@gmail.com>
2021-10-03 23:03:53 -0500

    update api/v2

--- api/v2/includes/functions.php
+++ api/v2/includes/functions.php
@@ -2,19 +2,7 @@
 //Campo ID
 function Identificador(){
 global $tabla;
-    switch (true) {
-        case ($tabla=='citas'):
-            $ID='CitaId';
-        break;
-        case ($tabla=='pacientes'):
-            $ID='PacienteId';
-        break;
-        case ($tabla=='usuarios'):
-            $ID='UsuarioId';
-        break;
-        case ($tabla=='usuarios_token'):
-            $ID='TokenId';
-        break;
+    switch ($tabla) {
         default:
             $ID='ID';
         break;
