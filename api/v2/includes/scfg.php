e42cd1f146061402342716b8aab799d0e0dfcc25
memojl <memojl08@gmail.com>
2021-12-30 11:06:25 -0600

    update api 30.12.2021

---
+++ api/v2/includes/scfg.php
@@ -0,0 +1,45 @@
+<?php 
+$h_s='linksm.herokuapp.com';
+if($_SERVER['HTTP_HOST']==$h_s || $_SERVER['HTTP_HOST']=='www.'.$h_s){
+    // Localhost Production
+    $db_host = "";
+    // Database Name      
+    $db_base = "db_links"; 
+    // Database Username     
+    $db_user = "";
+    // Database Password       
+    $db_pass = "";     	
+}else{
+    // Localhost Development
+    $db_host = "localhost";
+    $db_base = "db_links";
+    $db_user = "root"; 
+    $db_pass = "";     	        
+}
+// Settings
+$config = [
+    "driver" => "sqlite",
+    "host" => $db_host,
+    "database" => $db_base,
+    "username" => $db_user,
+    "password" => $db_pass,
+    "port" => "3306",
+    "charset" => "utf8mb4"
+];
+// Prefix for the database tables.
+$DBprefix = "";
+// Project root path		
+$path_root = ($_SERVER['HTTP_HOST']=='localhost')?'MisSitios/links/':'';
+// Database name and path for sqlite
+$rut = (basename($_SERVER['PHP_SELF'])=='index.php')?'../':'../../';
+$dbSQLite=$rut.'db/'.$config['database'].'.db';
+
+/*DEFINITION OF VARIABLES FOR PHP7*/
+define('DB_DRIVER',$config['driver']);
+define('DB_HOST',$config['host']);
+define('DB_USER',$config['username']);
+define('DB_PASSWORD',$config['password']);
+define('DB_DB',$config['database']);
+define('DB_PORT',$config['port']);
+define('DB_CHARSET',$config['charset']);
+?>
