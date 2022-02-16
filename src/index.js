7eda122e7036810b8d893b8943b7f910f2a68a23
memojl <memojl08@gmail.com>
2022-02-16 17:22:16 -0600

    update sesion

--- src/index.js
+++ src/index.js
@@ -11,13 +11,13 @@ import './assets/bootstrap-5.0.2/js/bootstrap.bundle.min.js';
 import {consola} from './app/console.js';
 import {inicio,variables} from './app/lib.js';
 import {reMod,menuWeb,consoleLocal} from './app/functions.js';
-import {no_menu,routes_session,router,controlRoutes} from './routes/index.routes.js';
+import {no_menu,menu_web,routes_session,router,controlRoutes} from './routes/index.routes.js';
 
 
 window.addEventListener('hashchange',()=>{consoleLocal('warn','Event Listener');
     const v = variables();
     const {hash,URL,pag_name,vars_Url,mod,ext,id,ext2,route,hash2,url_mod,url_m,url404} = v;
-    menuWeb(hash2,no_menu,routes_session);
+    menuWeb(hash2,no_menu,menu_web);
     router(hash,hash2,url_mod,url404);
     controlRoutes(route,id);
 
