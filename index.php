0bef37f01ee24284cb622b328608189a956b4285
memojl <memojl08@gmail.com>
2022-02-16 19:14:12 -0600

    Api for links

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>API - SQLITE</title>
+</head>
+<body>
+    <a href="/api/v2/">Ir a Api</a>
+</body>
+</html>
