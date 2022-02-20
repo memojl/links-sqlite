01f9ef541b0ad117aaa2a6d1e2df50745fa8f7c5
memojl <memojl08@gmail.com>
2022-02-19 20:51:08 -0600

    update mysql 19

--- api/v1/includes/scfg.php
+++ api/v1/includes/scfg.php
@@ -12,7 +12,7 @@ $db_user = "root";        	// Usuario de la Base de Datos
 $db_pass = "";     	// Password de la Base de Datos
 }
 $config = [
-    "driver" => "sqlite",
+    "driver" => "mysql",
     "host" => $db_host,
     "database" => $db_base,
     "username" => $db_user,
