bae9ee8065d311f4c34d64504620beac251da8d3
memojl <memojl08@gmail.com>
2022-02-19 21:48:26 -0600

    update 19.02.2022

--- api/rest/src/config.php
+++ api/rest/src/config.php
@@ -1,10 +1,10 @@
 <?php
 
 $db = [
-    'host' => 'us-cdbr-east-03.cleardb.com',
-    'username' => 'b6e038dc526164',
-    'password' => '9ee0c2b4',
-    'db' => 'heroku_4508b8af49308d8' //Cambiar al nombre de tu base de datos
+    'host' => 'localhost',
+    'username' => 'root',
+    'password' => '',
+    'db' => 'mandragora' //Cambiar al nombre de tu base de datos
 ];
 
 ?>
